b9a80bb47293b9f8605f83edb02d97e5205238f6
Calvin Bonomo <cal.bonomo@gmail.com>
2024-12-21 17:53:27 -0700

    Reworked init to be consistent with other functions

--- src/linked_list/LinkedList.h
+++ src/linked_list/LinkedList.h
@@ -15,8 +15,10 @@ typedef struct LinkedListNode
 } llnode_t;
 
 
-linkedList_t Init(float value);
+void Init(linkedList_t* list, float value);
+
 void Free(linkedList_t* list);
+llnode_t llFreeHelper(llnode_t* root);
 
 float Get(linkedList_t* list, int index);
 void Set(linkedList_t* list, int index, float value);
@@ -28,5 +30,6 @@ int Delete(linkedList_t* list, float value);
 float Pop(linkedList_t* list);
 
 void Sort(linkedList_t* list);
+
 
 #endif
